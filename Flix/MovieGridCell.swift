fac5428ccc6bced374da060efa9c4e8e6fe9fa88
alene006 <aleneestif@gmail.com>
2020-09-27 00:08:54 -0500

    Add Tab and Nav bar controllers | Add collection view for superhero screen

---
+++ Flix/MovieGridCell.swift
@@ -0,0 +1,13 @@
+//
+//  MovieGridCell.swift
+//  Flix
+//
+//  Created by Estifanos Alene on 9/26/20.
+//
+
+import UIKit
+
+class MovieGridCell: UICollectionViewCell {
+    
+    @IBOutlet weak var posterView: UIImageView!
+}
